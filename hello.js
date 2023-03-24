3885a1b45ad81d47174dc54053656d9e071271af
alvinarkansas <alvinarkansas@gmail.com>
2023-03-24 22:17:37 +0700

    all features

---
+++ hello.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log("Hello")
+
+console.log(chalk.blue("Hello wolrd"))
---
+++ start.js
@@ -0,0 +1,13 @@
+const umur = 13
+
+// apabila umur 17 taun ke atas maka print 'sudah bisa bikin KTP'
+// apabila umur 10 - 17 taun maka print 'hampir bisa bikin KTP'
+// apabila umur kurang dari 10 taun maka print 'belum cukup umur'
+
+if (umur >= 17 ) {
+  console.log("sudah bisa bikin KTP");
+} else if (umur >= 10) {
+  console.log("hampir bisa bikin KTP");
+} else   {
+  console.log("belum cukup umur");
+}
